42916a35ceb0ae88858238e1dd9b958ee83cad18
ArthurChassaing <94675172+ArthurChassaing@users.noreply.github.com>
2022-12-16 12:58:41 +0100

    class sprite

---
+++ LemmingsGame/Classes/sprite.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "cocos2d.h"
+//#include "HelloWorldScene.h"
+
+class game;
+
+class sprite
+{
+public:
+	sprite();
+	~sprite();
+	void setSprite(game* scene);
+
+
+private:
+
+
+
+};
+
