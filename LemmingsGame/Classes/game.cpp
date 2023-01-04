5359f3faac4ba802a1658135ef53629a4cb7ab9f
Pandouiille <agouveia@gaming.tech>
2023-01-04 17:10:05 +0100

    Map

--- LemmingsGame/Classes/game.cpp
+++ LemmingsGame/Classes/game.cpp
@@ -28,20 +28,16 @@ bool game::init()
 
     CCLOG("in the second scene");
 
-    auto bgSprite = Sprite::create("F:/lemmings/LemmingsGame/Assets/Battleground1.png");
+ 
+   auto visibleSize = Director::getInstance()->getVisibleSize();
+   Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-    auto visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+   cocos2d::Size screenSize = cocos2d::Director::getInstance()->getOpenGLView()->getFrameSize();
+   cocos2d::Vec2 centerPos = cocos2d::Vec2(screenSize.width / 2.0f, screenSize.height / 2.0f);
 
-    cocos2d::Size screenSize = cocos2d::Director::getInstance()->getOpenGLView()->getFrameSize();
-    cocos2d::Vec2 centerPos = cocos2d::Vec2(screenSize.width / 2.0f, screenSize.height / 2.0f);
-
-    bgSprite->setContentSize(Size(1200, 800));
-    bgSprite->setAnchorPoint(cocos2d::Vec2(0.5f, 0.5f));
-    bgSprite->setPosition(cocos2d::Vec2(screenSize.width / 2.0f, screenSize.height / 2.0f));
-    bgSprite->setVisible(true);
-
-    this->addChild(bgSprite, 0);
+   auto map = TMXTiledMap::create("Map.tmx");
+   map->setScale(2.1);
+   this->addChild(map);
 
     return true;
 }
