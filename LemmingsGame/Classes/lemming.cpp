a7a55b1fa51fc0958f86c9c1c83c22bda78ded25
ArthurChassaing <94675172+ArthurChassaing@users.noreply.github.com>
2023-01-05 17:29:16 +0100

    ouiii

---
+++ LemmingsGame/Classes/lemming.cpp
@@ -0,0 +1,45 @@
+#include "lemming.h"
+#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+lemming::lemming()
+{
+    
+}
+
+lemming::~lemming()
+{
+}
+
+void lemming::setSprite(game* scene)
+{
+    auto spriteCache = SpriteFrameCache::getInstance();
+    spriteCache->addSpriteFramesWithFile("idle/idle.plist");
+
+    Vector<SpriteFrame*> frames;
+    frames.pushBack(spriteCache->getSpriteFrameByName("1.png"));
+    //frames.pushBack(spriteCache->getSpriteFrameByName("2.png"));
+    //frames.pushBack(spriteCache->getSpriteFrameByName("3.png"));
+    //frames.pushBack(spriteCache->getSpriteFrameByName("4.png"));
+    auto animation = Animation::createWithSpriteFrames(frames);
+    animation->setDelayPerUnit(0.2);
+    animation->setLoops(-1);
+    auto action = Animate::create(animation);
+
+    auto sprite = Sprite::createWithSpriteFrame(frames.front());
+    sprite->setPosition(200, 200);
+    sprite->setScale(2);
+    sprite->runAction(action);
+
+    auto physicsBody = PhysicsBody::createBox(sprite->getContentSize(), PhysicsMaterial(0, 1, 0));
+    physicsBody->setDynamic(true);
+    sprite->setPhysicsBody(physicsBody);
+
+    physicsBody->setGravityEnable(true);
+    //sprite->addComponent(physicsBody);  
+    sprite->getPhysicsBody()->setVelocity(Vec2(50, 0));
+
+    scene->addChild(sprite, 0, 1);
+}
+
---
+++ LemmingsGame/Classes/lemming.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "cocos2d.h"
+#include "vector"
+
+
+class game;
+
+class lemming : public cocos2d::Sprite
+{
+public:
+	lemming();
+	~lemming();
+	void setSprite(game* scene);
+	
+
+
+private:
+	
+	
+
+};
+
